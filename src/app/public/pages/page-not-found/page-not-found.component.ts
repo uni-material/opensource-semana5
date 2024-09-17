d5976780403003f3db1ef63dccb4f01e6f705e62
RominaMaita <u202213765@upc.edu.pe>
2024-09-16 21:52:31 -0500

    feat(page-not-found): add page not found.

---
+++ src/app/public/pages/page-not-found/page-not-found.component.ts
@@ -0,0 +1,38 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatButton} from "@angular/material/button";
+
+@Component({
+  selector: 'app-page-not-found',
+  standalone: true,
+  imports: [
+    MatButton
+  ],
+  templateUrl: './page-not-found.component.html',
+  styleUrl: './page-not-found.component.css'
+})
+export class PageNotFoundComponent implements OnInit {
+  //#region Atributes
+
+  protected invalidUrl: string;
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private router: Router = inject(Router);
+  //endregion
+
+  //#region Methods
+  constructor() {
+    this.invalidUrl = '';
+  }
+
+  //endregion
+
+    ngOnInit(): void {
+        this.invalidUrl = this.route.snapshot.url[0].path;
+    }
+
+    protected onNavigateHome(){
+      this.router.navigate(['home']).then();
+    }
+
+
+}
